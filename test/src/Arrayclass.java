8663e22f7510ec44efbc0318bf030bb323973607
Administrator <palki3289@gmail.com>
2019-04-29 20:40:51 -0700

    Arrayquestion

--- test/src/Arrayclass.java
+++ test/src/Arrayclass.java
@@ -1,13 +1,26 @@
+//Find the element that appears once in a sorted array
+// where all other elements appear twice one after another. Find that element in 0(n) complexity.
+
 public class Arrayclass {
 
-    public static  void search(int[] array, int i, int j)
-    {
-
+    public static int search(int[] array, int a, int b) {
+        for (int i = 0; i < b - 1; i += 2) {
+            if (array[i] != array[i + 1]) {
+                return array[i];
+            }
+        }
+        return 0;
     }
 
     public static void main(String[] args)
     {
-        int[] arr = {1, 1, 2, 4, 4, 5, 5, 6, 6};
-        search(arr, 0, arr.length-1);
+        int[] arr = {1, 1, 2,4, 4, 5, 5, 6, 6};
+        int result = search(arr, 0, arr.length-1);
+        if(result == 0)
+        {
+            System.out.println("No such element exists");
+        }
+        else
+            System.out.println("element is " + result);
     }
 }
