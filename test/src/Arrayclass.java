6c69bfd48258e027beda18abaabe408e6d962e09
Administrator <palki3289@gmail.com>
2019-04-29 20:28:53 -0700

    Arrayquestion

---
+++ test/src/Arrayclass.java
@@ -0,0 +1,13 @@
+public class Arrayclass {
+
+    public static  void search(int[] array, int i, int j)
+    {
+
+    }
+
+    public static void main(String[] args)
+    {
+        int[] arr = {1, 1, 2, 4, 4, 5, 5, 6, 6};
+        search(arr, 0, arr.length-1);
+    }
+}
